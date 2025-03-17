7a9fb92c2671a1f9d3b4c2d498c39454575ea0e1
SayliDev <artaxw1@gmail.com>
2025-03-17 07:52:30 +0100

    feat(card) : create card and filter component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
+import Filter from "./components/Filter.vue";
+import Card from "./components/Card.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <Filter />
 
-  <div>
-    <h1 class="text-3xl font-bold underline">hello world</h1>
+  <!-- Liste des Pokémon -->
+  <div class="container mx-auto px-4 py-8">
+    <h2 class="text-2xl font-bold mb-6 text-center">Pokémon populaires</h2>
+
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
+    >
+      <Card />
+    </div>
   </div>
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card pokemon-card bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10 bg-base-200 rounded-t-2xl">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        alt="Pikachu"
+        class="w-32 h-32"
+      />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Pikachu</h2>
+      <div class="flex gap-2 my-1">
+        <div class="badge badge-neutral">Électrik</div>
+      </div>
+      <p>#25</p>
+      <div class="card-actions">
+        <button class="btn btn-primary btn-sm gap-1">
+          <PlusIcon class="w-5 h-5" /> Ajouter à l'équipe
+        </button>
+        <button class="btn btn-ghost btn-sm">
+          <Info class="w-5 h-5" /> Détails
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PlusIcon, Info } from "lucide-vue-next";
+</script>
+
+<style></style>
