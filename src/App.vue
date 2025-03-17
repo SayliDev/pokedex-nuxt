f27d02b4a7c52c11f3203a30f142c96af7ad0270
SayliDev <artaxw1@gmail.com>
2025-03-17 08:29:25 +0100

    feat(list) : add pokemon list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,55 @@
-<script setup lang="ts">
+<script setup>
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import Filter from "./components/Filter.vue";
 import Card from "./components/Card.vue";
+import { onMounted, ref } from "vue";
+
+const pokemons = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=40");
+    const data = await response.json();
+
+    const pokemonDetails = await Promise.all(
+      data.results.map(async ({ url }) => {
+        const res = await fetch(url);
+        const details = await res.json();
+
+        const speciesRes = await fetch(
+          `https://pokeapi.co/api/v2/pokemon-species/${details.id}/`
+        );
+        const speciesData = await speciesRes.json();
+
+        const frenchName =
+          speciesData.names.find((n) => n.language.name === "fr")?.name ||
+          details.name;
+
+        const pokemonData = {
+          id: details.id,
+          name: frenchName,
+          sprite: details.sprites.other.home.front_default,
+          types: details.types.map(({ type }) => type.name),
+          height: details.height,
+          weight: details.weight,
+          abilities: details.abilities.map(({ ability }) => ability.name),
+          stats: details.stats.map(({ stat, base_stat }) => ({
+            name: stat.name,
+            value: base_stat,
+          })),
+        };
+
+        return pokemonData;
+      })
+    );
+
+    pokemons.value = pokemonDetails;
+    console.log(pokemons.value);
+  } catch (error) {
+    console.error("Erreur lors du chargement des Pokémon :", error);
+  }
+});
 </script>
 
 <template>
@@ -17,7 +64,7 @@ import Card from "./components/Card.vue";
     <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
     >
-      <Card />
+      <Card v-for="pokemon in pokemons" :pokemon="pokemon" />
     </div>
   </div>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="card pokemon-card bg-base-100 shadow-xl">
     <figure class="px-10 pt-10 bg-base-200 rounded-t-2xl">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-        alt="Pikachu"
-        class="w-32 h-32"
-      />
+      <img :src="pokemon.sprite" :alt="pokemon.name" class="w-32 h-32" />
     </figure>
     <div class="card-body items-center text-center">
-      <h2 class="card-title">Pikachu</h2>
+      <h2 class="card-title">{{ pokemon.name }}</h2>
       <div class="flex gap-2 my-1">
-        <div class="badge badge-neutral">Électrik</div>
+        <div
+          class="badge badge-outline capitalize"
+          v-for="type in pokemon.types"
+        >
+          {{ type }}
+        </div>
       </div>
-      <p>#25</p>
+      <p>#{{ pokemon.id }}</p>
       <div class="card-actions">
         <button class="btn btn-primary btn-sm gap-1">
           <PlusIcon class="w-5 h-5" /> Ajouter à l'équipe
@@ -27,6 +28,13 @@
 
 <script setup lang="ts">
 import { PlusIcon, Info } from "lucide-vue-next";
+
+defineProps({
+  pokemon: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
 
 <style></style>
