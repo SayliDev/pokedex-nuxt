121bb4a7abd5078835effac8f16204948ce8213e
SayliDev <artaxw1@gmail.com>
2025-03-17 07:20:03 +0100

    feat(navbar): create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
 
 <template>
+  <Navbar />
   <div>
     <h1 class="text-3xl font-bold underline">hello world</h1>
   </div>
