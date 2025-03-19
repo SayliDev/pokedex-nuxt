f73f763b6473cb1ce75aee60d699ceef417211e5
SayliDev <artaxw1@gmail.com>
2025-03-19 12:38:26 +0100

    fix: remove duplicate Hero component and implement DrawerHandle functionality
    - Removed Hero component from App.vue to avoid duplication in the index page.
    - Implemented DrawerHandle functionality to toggle TeamDrawer visibility using store state.

--- app.vue
+++ app.vue
@@ -13,21 +13,16 @@ if (import.meta.client) {
 </script>
 
 <template>
-  <html lang="fr" data-theme="bumblebee">
-    <body data-theme="bumblebee">
-      <Navbar />
-
-      <NuxtPage />
-
-      <DrawerHandle />
-
-      <Toast
-        :message="teamStore.toastMessage"
-        :toast-type="teamStore.toastType"
-        :show-toast="teamStore.showToast"
-        @hide="teamStore.hideToast"
-      />
-      <TeamDrawer />
-    </body>
-  </html>
+  <div>
+    <Navbar />
+    <NuxtPage />
+    <DrawerHandle />
+    <Toast
+      :message="teamStore.toastMessage"
+      :toast-type="teamStore.toastType"
+      :show-toast="teamStore.showToast"
+      @hide="teamStore.hideToast"
+    />
+    <TeamDrawer />
+  </div>
 </template>
