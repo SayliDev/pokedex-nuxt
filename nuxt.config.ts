0cc2de89b70dd25b691bca9921092a4924621a39
SayliDev <artaxw1@gmail.com>
2025-03-19 12:21:02 +0100

    feat: update tailwindcss and daisyui dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
